b1fbf0572128d62cecae0b1af0d2314fefee1d1a
Timo Kluck <tkluck@infty.nl>
2017-05-11 01:37:19 +0200

    Make CONNECT work

--- main.py
+++ main.py
@@ -5,29 +5,57 @@ from twisted.web.client import Agent, FileBodyProducer, Headers, readBody
 from twisted.internet import defer
 from twisted.internet.defer import inlineCallbacks
 
+from argparse import ArgumentParser
+
+import requests
+import pacparser
+
+parser= ArgumentParser(description="""
+Run a simple HTTP proxy on localhost that uses a wpad.dat to decide
+how to connect to the actual server.
+""")
+parser.add_argument("-c", "--config", type=str)
+parser.add_argument("-p", "--port", type=int, metavar="PORT")
+parser.add_argument("-F", "--force-proxy", type=str, metavar="PROXY STRING")
+
+args= parser.parse_args()
+
+
 from proxy import WPADProxyRequest
 
 import logging
 logger = logging.getLogger('env-pac')
 
-def start_server(reactor):
+def start_server(port, reactor):
     logger.info("Starting proxy server...")
 
     factory = HTTPFactory()
     factory.protocol = proxy.Proxy
     factory.protocol.requestFactory = WPADProxyRequest
 
-    port = reactor.listenTCP(3128, factory)
+    port = reactor.listenTCP(port, factory)
 
     logger.info("...proxy server started")
 
-def main():
+def main(args):
     try:
-        start_server(reactor)
+        pacparser.init()
+        if args.force_proxy:
+            WPADProxyRequest.force_proxy = args.force_proxy
+        else:
+            pacparser.parse_pac_string(requests.get(args.config).text)
+        start_server(args.port, reactor)
     except Exception as e:
         logger.error("Problem starting the server", exc_info=True)
 
 if __name__ == "__main__":
-    main()
+    import os
+    log_level_name = os.environ.get('LOG_LEVEL', 'info')
+    log_level = getattr(logging, log_level_name.upper(), logging.INFO)
+    log_handler = logging.StreamHandler()
+    logger.setLevel(log_level)
+    logger.addHandler(log_handler)
+    log_handler.setFormatter(logging.Formatter(fmt="%(levelname)s [%(process)d]: %(name)s: %(message)s"))
+    main(args)
     reactor.run()
     logger.info("Shutdown")
--- proxy.py
+++ proxy.py
@@ -17,141 +17,123 @@ from twisted.protocols import portforward
 logger = logging.getLogger('env-pac')
 
 import pacparser
-import requests
-pacparser.init()
-pacparser.parse_pac_string(requests.get("http://wpad.booking.pcln.com/wpad.dat").text)
 
 class WPADProxyRequest(proxy.ProxyRequest):
+
+    force_proxy = None
+
     def process(self):
-            if self.method == b'CONNECT':
-                uri = self.uri.decode('ascii')
-                host, port = uri.split(":")
+        if self.method == b'CONNECT':
+            uri = self.uri.decode('ascii')
+            host, port = uri.split(":")
+            port = int(port)
+        else:
+            parsed = urllib_parse.urlparse(self.uri)
+            host = parsed[1].decode('ascii')
+            if ':' in host:
+                host, port = host.split(':')
                 port = int(port)
-
-                protocol = b'http'
             else:
-                parsed = urllib_parse.urlparse(self.uri)
-                host = parsed[1].decode('ascii')
-                protocol = parsed[0] or b'http'
-                if ':' in host:
-                    host, port = host.split(':')
-                    port = int(port)
+                port = 80
+            rest = urllib_parse.urlunparse((b'', b'') + parsed[2:])
+            if not rest:
+                rest = rest + b'/'
+
+        headers = self.getAllHeaders().copy()
+        self.content.seek(0, 0)
+        s = self.content.read()
+
+        logger.debug("received {} request for {}".format(self.method, host))
+        proxy_suggestion = self.force_proxy or pacparser.find_proxy('http://{}'.format(host))
+        logger.debug("proxy: {}".format(proxy_suggestion))
+
+        if proxy_suggestion != 'DIRECT':
+            options = proxy_suggestion.split(";")
+            connect_method, destination = options[0].split(" ")
+            if connect_method == 'PROXY':
+                proxy_host, proxy_port = destination.split(":")
+                proxy_port = int(proxy_port)
+                logger.debug("proxying to {} at port {}".format(proxy_host, proxy_port))
+                if self.method != b'CONNECT':
+                    clientFactory = proxy.ProxyClientFactory(
+                        self.method,
+                        self.uri,
+                        self.clientproto,
+                        headers,
+                        s,
+                        self,
+                    )
                 else:
-                    port = self.ports[protocol]
-                rest = urllib_parse.urlunparse((b'', b'') + parsed[2:])
-                if not rest:
-                    rest = rest + b'/'
+                    self.transport.unregisterProducer()
+                    self.transport.pauseProducing()
+                    rawConnectionProtocol = portforward.Proxy()
+                    rawConnectionProtocol.transport = self.transport
+                    self.transport.protocol = rawConnectionProtocol
 
-            class_ = proxy.ProxyClientFactory
+                    clientFactory = CONNECTProtocolForwardFactory(host, port)
+                    clientFactory.setServer(rawConnectionProtocol)
 
-
-            headers = self.getAllHeaders().copy()
-            self.content.seek(0, 0)
-            s = self.content.read()
-
-            logger.debug("received {} request for {}".format(self.method, host))
-            proxy_suggestion = pacparser.find_proxy('{}://{}'.format(protocol, host))
-            logger.debug("proxy: {}".format(proxy_suggestion))
-
-            if proxy_suggestion != 'DIRECT':
-                options = proxy_suggestion.split(";")
-                method, destination = options[0].split(" ")
-                if method == 'PROXY':
-                    proxy_host, proxy_port = destination.split(":")
-                    proxy_port = int(proxy_port)
-                    logger.debug("proxying to {} at port {}".format(proxy_host, proxy_port))
-                    clientFactory = class_(self.method, self.uri, self.clientproto, headers,
-                                           s, self)
-                    self.reactor.connectTCP(proxy_host, proxy_port, clientFactory)
-                    #self.reactor.connectSSL(proxy_host, proxy_port,
-                    #                    clientFactory, ssl.optionsForClientTLS(host))
-                    return
-            if self.method != b'CONNECT':
-                if b'host' not in headers:
-                    headers[b'host'] = host.encode('ascii')
-
-                clientFactory = class_(self.method, rest, self.clientproto, headers,
-                                       s, self)
-                if protocol == b'http':
-                    self.reactor.connectTCP(host, port, clientFactory)
-                elif protocol == b'https':
-                    self.reactor.connectSSL(host, port,
-                                        clientFactory, ssl.optionsForClientTLS(host))
+                self.reactor.connectTCP(proxy_host, proxy_port, clientFactory)
+                return
             else:
-                clientFactory = portforward.ProxyClientFactory()
-                self.reactor.connectSSL(host, port,
-                                    clientFactory, ssl.optionsForClientTLS(host))
+                # can this be anything else? Let's fall back to the DIRECT
+                # codepath.
+                pass
+        if self.method != b'CONNECT':
+            if b'host' not in headers:
+                headers[b'host'] = host.encode('ascii')
 
-# ***
-# utility functions
-# ***
+            clientFactory = proxy.ProxyClientFactory(
+                self.method,
+                rest,
+                self.clientproto,
+                headers,
+                s,
+                self,
+            )
+            logger.debug("forwarding to http://{}:{}".format(host, port))
+            self.reactor.connectTCP(host, port, clientFactory)
+        else:
+            # hack/trick to move responsibility for this connecction
+            # away from a HTTP protocol class hierarchy and to a
+            # port forward hierarchy
+            self.transport.unregisterProducer()
+            self.transport.pauseProducing()
+            rawConnectionProtocol = portforward.Proxy()
+            rawConnectionProtocol.transport = self.transport
+            self.transport.protocol = rawConnectionProtocol
 
-def with_timeout(time, reactor, deferred):
-    timeoutCall = reactor.callLater(time, deferred.cancel)
-    def completed(passthrough):
-        if timeoutCall.active():
-            timeoutCall.cancel()
-        return passthrough
-    deferred.addBoth(completed)
-    return deferred
+            clientFactory = portforward.ProxyClientFactory()
+            clientFactory.setServer(rawConnectionProtocol)
+            clientFactory.protocol = CONNECTProtocolClient
+            # we don't do connectSSL, as the handshake is taken
+            # care of by the client, and we only forward it
+            self.reactor.connectTCP(host, port,
+                                clientFactory)
 
-def do_periodically(time, reactor, async_call):
-    # Implementation note:
-    # it would be great if current_d could just
-    # be  a local variable, but python scoping
-    # doesn't allow that (it becomes a different local
-    # variable inside do_next_call, because do_next_call
-    # assigns to it). That's why I'm using a workaround
-    # with a dictionary element. Putting the intended
-    # code in comments.
-    # Python3 solves this with the 'nonlocal' keyword
-    info = { 'current_d': None }             # current_d = None
-    def do_next_call():                      # def do_next_call()
-        if info['current_d']:                #     if current_d:
-            info['current_d'].cancel()       #         current_d.cancel
-        info['current_d'] = async_call()     #     current_d = async_call()
-        reactor.callLater(time, do_next_call)
-    reactor.callLater(time, do_next_call)
 
-def sleep_async(time, reactor, result=None):
-    d = Deferred()
-    timer = reactor.callLater(time, lambda: d.callback(result))
-    def on_error(err):
-        timer.cancel()
-        raise err
-    d.addErrback(on_error)
-    return d
+class CONNECTProtocolClient(portforward.ProxyClient):
+    def connectionMade(self):
+        self.peer.transport.write(b"HTTP/1.1 200 OK\r\n\r\n")
+        portforward.ProxyClient.connectionMade(self)
 
-class CleanShutdown(object):
-    def __init__(self, reactor, port):
-        self.port = port
-        self.clean_exit_deferred = Deferred()
-        self.outstanding_waits = 0
-        self.stop_listening = None
 
-        reactor.addSystemEventTrigger('before', 'shutdown', self._before_shutdown)
+class CONNECTProtocolForward(portforward.ProxyClient):
+    def connectionMade(self):
+        self.transport.write(
+                "CONNECT {}:{} HTTP/1.1\r\nhost: {}\r\n\r\n".format(
+                self.factory.host,
+                self.factory.port,
+                self.factory.host,
+            ).encode('ascii')
+        )
+        portforward.ProxyClient.connectionMade(self)
 
-    def _before_shutdown(self):
-        self.stop_listening = self.port.stopListening()
-        self.stop_listening.addBoth(self._maybe_clean_exit)
-        return self.clean_exit_deferred
+class CONNECTProtocolForwardFactory(portforward.ProxyClientFactory):
+    protocol = CONNECTProtocolForward
+    def __init__(self, host, port):
+            portforward.ProxyClientFactory.__init__(self)
+            self.host = host
+            self.port = port
 
-    def wait_for(self, d):
-        self.outstanding_waits += 1
-        def on_both(result):
-            self.outstanding_waits -= 1
-            self._maybe_clean_exit(None)
-        d.addBoth(on_both)
-        return d
 
-    def _maybe_clean_exit(self, result):
-        if self.stop_listening and self.stop_listening.called and self.outstanding_waits == 0:
-            self.clean_exit_deferred.callback(None)
-
-import os
-log_level_name = os.environ.get('LOG_LEVEL', 'info')
-log_level = getattr(logging, log_level_name.upper(), logging.INFO)
-log_handler = logging.StreamHandler()
-logger.setLevel(log_level)
-logger.addHandler(log_handler)
-log_handler.setFormatter(logging.Formatter(fmt="%(levelname)s [%(process)d]: %(name)s: %(message)s"))
