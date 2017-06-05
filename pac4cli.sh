0f099f7f08d1c9fa0cbf0d41d8afa4453ad7ed77
Khalid Eldehairy <kdehairy@gmail.com>
2017-06-05 12:01:43 +0200

    Cover java with the proxy setting
    add the proxy settings to _JAVA_OPTIONS too.

--- pac4cli.sh
+++ pac4cli.sh
@@ -3,3 +3,7 @@ http_proxy=http://localhost:3128
 https_proxy=http://localhost:3128
 export http_proxy
 export https_proxy
+
+_JAVA_OPTIONS="${_JAVA_OPTIONS} -Dhttp.proxyHost=${http_proxy%:*} -Dhttp.proxyPort=${http_proxy##*:} -Dhttps.proxyHost=${https_proxy%:*} -Dhttps.proxyPort=${https_proxy##*:}"
+
+export _JAVA_OPTIONS
