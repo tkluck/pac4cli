b8c0912bf5ef39e8545a3c6f5dc0fc21359cee80
Khalid Eldehairy <khalid@kdehairy.com>
2017-06-06 13:36:02 +0200

    Fixing _JAVA_OPTIONS http(s) host
    java's system property http(s).proxyHost expects the host part only from the url. no protocol.

--- pac4cli.sh
+++ pac4cli.sh
@@ -4,6 +4,12 @@ https_proxy=http://localhost:3128
 export http_proxy
 export https_proxy
 
-_JAVA_OPTIONS="${_JAVA_OPTIONS} -Dhttp.proxyHost=${http_proxy%:*} -Dhttp.proxyPort=${http_proxy##*:} -Dhttps.proxyHost=${https_proxy%:*} -Dhttps.proxyPort=${https_proxy##*:}"
+http_host=${http_proxy%:*}
+http_host=${http_proxy##*/}
+
+https_host=${https_proxy%:*}
+https_host=${https_proxy##*/}
+
+_JAVA_OPTIONS="${_JAVA_OPTIONS} -Dhttp.proxyHost=${http_host} -Dhttp.proxyPort=${http_proxy##*:} -Dhttps.proxyHost=${https_host} -Dhttps.proxyPort=${https_proxy##*:}"
 
 export _JAVA_OPTIONS
