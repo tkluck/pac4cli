35a37ef4b9832daf1d64b4f5452864b5b47c5a43
Khalid Eldehairy <khalid@kdehairy.com>
2017-06-06 13:44:53 +0200

    Fixing again _JAVA_OPTIONS http host
    The previous fix did not properly trim the port from the url.
    
    This is what I get from trusting my skills and do no testing :(

--- pac4cli.sh
+++ pac4cli.sh
@@ -5,10 +5,10 @@ export http_proxy
 export https_proxy
 
 http_host=${http_proxy%:*}
-http_host=${http_proxy##*/}
+http_host=${http_host##*/}
 
 https_host=${https_proxy%:*}
-https_host=${https_proxy##*/}
+https_host=${https_host##*/}
 
 _JAVA_OPTIONS="${_JAVA_OPTIONS} -Dhttp.proxyHost=${http_host} -Dhttp.proxyPort=${http_proxy##*:} -Dhttps.proxyHost=${https_host} -Dhttps.proxyPort=${https_proxy##*:}"
 
