8acaf2dcc3807c404dc4663d81059fd94448e79f
Timo Kluck <tkluck@infty.nl>
2017-08-29 15:06:20 +0200

    Support both 'official' systemd and mosquito systemd python packages

--- servicemanager.py
+++ servicemanager.py
@@ -1,16 +1,18 @@
 import platform
 import logging
 
+LogHandler = logging.NullHandler
+def notify_ready():
+    pass
+
 if 'Linux' == platform.system():
     import systemd.daemon
     import systemd.journal
-
-def notify_ready():
-    if 'Linux' == platform.system():
-        systemd.daemon.notify(systemd.daemon.Notification.READY)
-
-def getLogHandler():
-    if 'Linux' == platform.system():
-        return systemd.journal.JournaldLogHandler()
-    else:
-        return logging.NullHandler()
+    if hasattr(systemd.journal, 'JournalHandler'):       # official bindings
+        LogHandler = systemd.journal.JournalHandler
+        def notify_ready():
+            systemd.daemon.notify("READY=1")
+    elif hasattr(systemd.journal, 'JournaldLogHandler'): # mosquito bindings
+        LogHandler = systemd.journal.JournaldLogHandler
+        def notify_ready():
+            systemd.daemon.notify(systemd.daemon.Notification.READY)
