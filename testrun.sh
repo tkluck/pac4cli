e2fd8a091905653fb52f8dbce62cd2ce52804836
Timo Kluck <tkluck@infty.nl>
2018-03-17 16:25:59 +0100

    Allow more time for the proxy to come up
    This is needed in the CircleCI container.

--- testrun.sh
+++ testrun.sh
@@ -7,7 +7,7 @@ env/bin/python main.py -p $PORT1  -F DIRECT &
 PID1=$!
 env/bin/python main.py -p $PORT2 -F "PROXY localhost:$PORT1" &
 PID2=$!
-sleep 0.3
+sleep 5
 
 (
     http_proxy=localhost:$PORT1 curl http://booking.com &&
