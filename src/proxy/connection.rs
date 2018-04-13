04d44dc425101776eafd261732ce1ed6b628434d
Timo Kluck <tkluck@infty.nl>
2018-04-13 20:19:42 +0200

    Implement my own two_way_pipe so I can handle (dis)connection logic
    Before this commit, pac4cli-rust would just hold on to all sockets
    indefinitely.
    
    There's still issues: the current logic doesn't guarantee that anything
    that's in the buffer will still be sent downstream if upstream closes
    the connection. Also, this way, we will *always* end with an error
    condition, even if at the http level, we're good.

--- src/proxy/connection.rs
+++ src/proxy/connection.rs
@@ -6,6 +6,8 @@ use tokio::prelude::*;
 use tokio_io::io::{ReadHalf,WriteHalf};
 
 use std::mem;
+use std;
+use std::cmp::min;
 
 use super::protocol::Preamble;
 
@@ -92,11 +94,176 @@ impl<IO: AsyncRead> Future for Incoming<IO> {
     }
 }
 
-pub fn two_way_pipe<T,S>(t:T, s:S) -> future::Join<io::Copy<ReadHalf<T>,WriteHalf<S>>,io::Copy<ReadHalf<S>,WriteHalf<T>>>
+struct RingBuffer<T> {
+    buf: Vec<T>,
+    start: usize,
+    length: usize,
+}
+
+impl<T> RingBuffer<T> {
+    pub fn new(buf: Vec<T>) -> Self {
+        RingBuffer { buf, start: 0, length: 0 }
+    }
+    pub fn with_next_writeable_chunk<F>(&mut self, f: F) -> Result<(),std::io::Error>
+        where F: FnOnce(&mut [T]) -> Result<usize,std::io::Error>
+    {
+        if self.length < self.buf.len() {
+            let begin_chunk = (self.start + self.length) % self.buf.len();
+            let end_chunk = if self.start > begin_chunk {
+                self.start
+            } else {
+                self.buf.len()
+            };
+            if begin_chunk < end_chunk {
+                let written = f(&mut self.buf[begin_chunk..end_chunk])?;
+                self.length += written;
+                if self.length > self.buf.len() {
+                    panic!("Producing more than the entire ring buffer");
+                }
+            }
+        }
+        Ok(())
+    }
+    pub fn with_next_readable_chunk<F>(&mut self, f: F) -> Result<(),std::io::Error>
+        where F: FnOnce(&[T]) -> Result<usize,std::io::Error>
+    {
+        let end_chunk = min(self.start + self.length, self.buf.len());
+        if end_chunk > self.start {
+            let read = f(&self.buf[self.start..end_chunk])?;
+            if read > self.length {
+                panic!("Consuming more than the entire ring buffer");
+            }
+            self.start = (self.start + read) % self.buf.len();
+            self.length -= read;
+        }
+        Ok(())
+    }
+}
+
+const RINGBUFFER_SIZE : usize = 64*1024;
+
+pub struct TwoWayPipe<T: AsyncRead+AsyncWrite,S: AsyncRead+AsyncWrite> {
+    t: T,
+    s: S,
+    t_to_s_buf: RingBuffer<u8>,
+    s_to_t_buf: RingBuffer<u8>,
+}
+
+impl<T: AsyncRead+AsyncWrite,S: AsyncRead+AsyncWrite> TwoWayPipe<T, S> {
+    pub fn new(t: T, s: S) -> Self {
+        TwoWayPipe {
+            t, s,
+            t_to_s_buf: RingBuffer::new([0u8; RINGBUFFER_SIZE].to_vec()),
+            s_to_t_buf: RingBuffer::new([0u8; RINGBUFFER_SIZE].to_vec()),
+        }
+    }
+}
+
+fn zero_op() -> io::Error {
+    io::Error::new(io::ErrorKind::WriteZero, "zero-length operation")
+}
+
+impl<T: AsyncRead+AsyncWrite,S: AsyncRead+AsyncWrite> Future for TwoWayPipe<T, S> {
+    type Item = ();
+    type Error = std::io::Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        let s = &mut self.s;
+        let t = &mut self.t;
+        loop {
+            let mut none_are_ready = true;
+            self.t_to_s_buf.with_next_writeable_chunk(|next_writeable_chunk| {
+                match t.poll_read(next_writeable_chunk) {
+                    Ok(Async::Ready(0)) => {
+                        t.shutdown();
+                        s.shutdown();
+                        Err(zero_op())
+                    }
+                    Ok(Async::Ready(n)) => {
+                        none_are_ready = false;
+                        Ok(n)
+                    }
+                    Ok(Async::NotReady) => {
+                        Ok(0)
+                    }
+                    Err(err) => {
+                        t.shutdown();
+                        s.shutdown();
+                        Err(err)
+                    }
+                }
+            })?;
+            self.t_to_s_buf.with_next_readable_chunk(|next_readable_chunk| {
+                match s.poll_write(next_readable_chunk) {
+                    Ok(Async::Ready(0)) => {
+                        t.shutdown();
+                        s.shutdown();
+                        Err(zero_op())
+                    }
+                    Ok(Async::Ready(n)) => {
+                        none_are_ready = false;
+                        Ok(n)
+                    }
+                    Ok(Async::NotReady) => {
+                        Ok(0)
+                    },
+                    Err(err) => {
+                        t.shutdown();
+                        s.shutdown();
+                        Err(err)
+                    }
+                }
+            })?;
+            self.s_to_t_buf.with_next_writeable_chunk(|next_writeable_chunk| {
+                match s.poll_read(next_writeable_chunk) {
+                    Ok(Async::Ready(0)) => {
+                        t.shutdown();
+                        s.shutdown();
+                        Err(zero_op())
+                    }
+                    Ok(Async::Ready(n)) => {
+                        none_are_ready = false;
+                        Ok(n)
+                    }
+                    Ok(Async::NotReady) => Ok(0),
+                    Err(err) => {
+                        t.shutdown();
+                        s.shutdown();
+                        Err(err)
+                    }
+                }
+            })?;
+            self.s_to_t_buf.with_next_readable_chunk(|next_readable_chunk| {
+                match t.poll_write(next_readable_chunk) {
+                    Ok(Async::Ready(0)) => {
+                        t.shutdown();
+                        s.shutdown();
+                        Err(zero_op())
+                    }
+                    Ok(Async::Ready(n)) => {
+                        none_are_ready = false;
+                        Ok(n)
+                    }
+                    Ok(Async::NotReady) => Ok(0),
+                    Err(err) => {
+                        t.shutdown();
+                        s.shutdown();
+                        Err(err)
+                    }
+                }
+            })?;
+            if none_are_ready {
+                return Ok(Async::NotReady)
+            }
+        }
+    }
+}
+
+pub fn two_way_pipe<T,S>(t:T, s:S) -> Box<Future<Item=(),Error=std::io::Error>+Send>
 where
-    T: AsyncRead+AsyncWrite,
-    S: AsyncRead+AsyncWrite {
-    let (t_read, t_write) = t.split();
-    let (s_read, s_write) = s.split();
-    return io::copy(t_read,s_write).join(io::copy(s_read,t_write));
+    T: AsyncRead+AsyncWrite+Send+'static,
+    S: AsyncRead+AsyncWrite+Send+'static {
+
+    let pipe = TwoWayPipe::new(t,s);
+    Box::new(pipe)
 }
