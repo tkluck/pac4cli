6d129018e266f896a7fae08726b8d777639b1d4c
Khalid Eldehairy <khalid@kdehairy.com>
2018-08-12 22:01:08 +0200

    Create a Docker container with systemd
    by running `test/test-arch-package.sh` a docker image with systemd init
    running is built and a container is run.
    
    The building of the image in itself is testing if the PKGBUILD is
    successful in describing the build/install for `makepkg`.
    
    The container is running pac4cli as a systemd service.
    
    Next step is running tests inside on a native installation.

---
+++ test/test-arch-package.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+SRC_DIR=$1
+
+if [[ "x${SRC_DIR}" == "x" ]]; then
+	echo "Usage:"
+	echo "  test-arch-package <project_src_dir>"
+	exit 1
+fi
+
+cd $SRC_DIR
+
+sudo docker build -t pac4cli -f archlinux/Dockerfile .
+
+#systemd expects:
+# - /run to be a tmpfs
+# - /sys/fs/cgroup mounted (read only is enough)
+# - Not sure about the `--tmpfs /tmp`. But without it systemd is unable to mount
+# to /tmp. It seems that systemd needs /tmp to be a tmpfs
+#
+# TODO: Currently this is a kind of noop container. I need to figure out how to
+# start it with a command (test script).
+sudo docker run -d --tmpfs /tmp --tmpfs /run -v /sys/fs/cgroup:/sys/fs/cgroup:ro pac4cli
