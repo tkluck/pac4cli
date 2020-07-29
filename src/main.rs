23235632c51b4da8e0c6d94e692282c07b982a6e
Timo Kluck <tkluck@infty.nl>
2020-07-29 22:39:42 +0100

    Restore SIGHUP behaviour

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::sync::Arc;
 
 #[macro_use]
@@ -10,7 +10,8 @@ use argparse::{ArgumentParser, StoreTrue, Store, StoreOption};
 use ini::Ini;
 use slog::Drain;
 use slog_journald::JournaldDrain;
-use tokio_signal::unix::{Signal, SIGHUP};
+use tokio::signal::unix::{signal, SignalKind};
+use tokio::net::TcpListener;
 
 mod pacparser;
 mod proxy;
@@ -20,50 +21,72 @@ mod systemd;
 use pacparser::{ProxySuggestion,find_proxy_suggestions};
 use wpad::AutoConfigHandler;
 
+#[derive(Clone)]
 struct Options {
     config: Option<String>,
     port: u16,
     force_proxy: Option<ProxySuggestion>,
+    force_wpad_url: Option<String>,
     loglevel: slog::FilterLevel,
     systemd: bool,
 }
 
 #[tokio::main]
 async fn main() {
-    let mut options = Options {
-        config:      None,
-        port:        3128,
-        force_proxy: None,
-        loglevel:    slog::FilterLevel::Debug,
-        systemd:     false,
-    };
 
-    {  // this block limits scope of borrows by ap.refer() method
-        let mut ap = ArgumentParser::new();
-        ap.set_description("
-        Run a simple HTTP proxy on localhost that uses a wpad.dat to decide
-        how to connect to the actual server.
-        ");
-        ap.refer(&mut options.config)
-            .add_option(&["-c", "--config"], StoreOption,
-            "Path to configuration file");
-        ap.refer(&mut options.port)
-            .metavar("PORT")
-            .add_option(&["-p","--port"], Store,
-            "Port to listen on");
-        ap.refer(&mut options.force_proxy)
-            .metavar("PROXY STRING")
-            .add_option(&["-F", "--force-proxy"], StoreOption,
-            "Forward traffic according to PROXY STRING, e.g. DIRECT or PROXY <proxy>");
-        ap.refer(&mut options.loglevel)
-            .metavar("LEVEL")
-            .add_option(&["--loglevel"], Store,
-            "One of DEBUG/INFO/WARNING/ERROR");
-        ap.refer(&mut options.systemd)
-            .add_option(&["--systemd"], StoreTrue,
-            "Assume running under systemd (log to journald)");
-        ap.parse_args_or_exit();
-    }
+    let options = {
+        let mut options = Options {
+            config:      None,
+            port:        3128,
+            force_proxy: None,
+            force_wpad_url: None,
+            loglevel:    slog::FilterLevel::Debug,
+            systemd:     false,
+        };
+        {
+            let mut ap = ArgumentParser::new();
+            ap.set_description("
+            Run a simple HTTP proxy on localhost that uses a wpad.dat to decide
+            how to connect to the actual server.
+            ");
+            ap.refer(&mut options.config)
+                .add_option(&["-c", "--config"], StoreOption,
+                "Path to configuration file");
+            ap.refer(&mut options.port)
+                .metavar("PORT")
+                .add_option(&["-p","--port"], Store,
+                "Port to listen on");
+            ap.refer(&mut options.force_proxy)
+                .metavar("PROXY STRING")
+                .add_option(&["-F", "--force-proxy"], StoreOption,
+                "Forward traffic according to PROXY STRING, e.g. DIRECT or PROXY <proxy>");
+            ap.refer(&mut options.loglevel)
+                .metavar("LEVEL")
+                .add_option(&["--loglevel"], Store,
+                "One of DEBUG/INFO/WARNING/ERROR");
+            ap.refer(&mut options.systemd)
+                .add_option(&["--systemd"], StoreTrue,
+                "Assume running under systemd (log to journald)");
+            ap.parse_args_or_exit();
+        }
+
+        options.force_wpad_url = if let Some(file) = options.config.clone() {
+            info!("Loading configuration file {}", file);
+            let conf = Ini::load_from_file(file).expect("Failed to load config file");
+            if let Some(section) = conf.section(Some("wpad".to_owned())) {
+                if let Some(url) = section.get("url") {
+                    Some(url.clone())
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        } else {
+            None
+        };
+        options.clone()
+    };
     // set up logging
     // Need to keep _guard alive for as long as we want to log
     let _guard = match options.systemd {
@@ -78,22 +101,6 @@ async fn main() {
             let log = slog::Logger::root(drain, slog_o!());
             slog_scope::set_global_logger(log)
         }
-    };
-
-    let force_wpad_url = if let Some(file) = options.config {
-        info!("Loading configuration file {}", file);
-        let conf = Ini::load_from_file(file).expect("Failed to load config file");
-        if let Some(section) = conf.section(Some("wpad".to_owned())) {
-            if let Some(url) = section.get("url") {
-                Some(url.clone())
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    } else {
-        None
     };
 
     pacparser::init().expect("Failed to initialize pacparser");
@@ -115,63 +122,48 @@ async fn main() {
             }
         }
     };
+    let find_proxy_arc = Arc::new(find_proxy);
 
+    update_configuration_from_wpad(&options, auto_config_handler.clone()).await;
 
-    /*
-    let handle_sighups : Box<dyn Future<Item=(),Error=()>> = {
-        if options.force_proxy.is_none() && force_wpad_url.is_none() {
-            let handle = core.handle();
-            let auto_config_handler = auto_config_handler.clone();
-            let task = Signal::new(SIGHUP, &handle).flatten_stream()
-            .map_err(|err| {
-                warn!("Error retrieving SIGHUPs: {:?}", err)
-            })
-            .and_then(move |_| {
-                let handle = handle.clone();
-                let auto_config_handler = auto_config_handler.clone();
-                info!("SIGHUP received");
-                wpad::get_wpad_urls(&handle)
-                .and_then(move |urls| {
-                    wpad::retrieve_first_working_url(&handle, urls)
-                })
-                .map(move |wpad| {
-                    auto_config_handler.set_current_wpad_script(wpad)
-                })
-            })
-            .for_each(|_| {
-                future::ok(())
-            })
-            .map_err(|err| {
-                warn!("Error handling SIGHUP: {:?}", err)
-            });
-            Box::new(task)
-        } else {
-            // if we get the proxy from arguments or the config file,
-            // no need to listen for SIGHUPs.
-            Box::new(future::ok(()))
-        }
-    };
-    */
+    let mut sighups = signal(SignalKind::hangup()).unwrap();
+    let mut sigints = signal(SignalKind::interrupt()).unwrap();
 
-    if options.force_proxy.is_none() {
-        let urls = match force_wpad_url {
-            Some(ref url) => [url.clone()].to_vec(),
-            None => wpad::get_wpad_urls().await.unwrap(),
-        };
-        let wpad = wpad::retrieve_first_working_url(urls).await.unwrap();
-        auto_config_handler.set_current_wpad_script(wpad)
-    }
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), options.port);
+    let mut listener = TcpListener::bind(&addr).await.unwrap();
 
-    // there is still a race condition here, as the socket is
-    // only bound lazily by tokio's futures/streams. The API has
-    // no way of hooking in to the moment that the socket has been
-    // bound (before any connections have been accepted), so this
-    // is as close as we'll get.
     systemd::notify_ready();
 
-    proxy::serve(options.port, find_proxy).await;
+    loop {
+        tokio::select! {
+            _ = sighups.recv() => {
+                update_configuration_from_wpad(&options, auto_config_handler.clone()).await;
+            }
+            _ = sigints.recv() => {
+                break;
+            }
+            Ok((downstream_connection, _)) = listener.accept() => {
+                debug!("accepted socket; addr={:?}", downstream_connection.peer_addr().unwrap());
+                let find_proxy_clone = find_proxy_arc.clone();
+                tokio::spawn(async move {
+                    proxy::process_socket(downstream_connection, find_proxy_clone).await;
+                });
+            }
+        }
+    }
 
     pacparser::cleanup();
 
     info!("Clean exit");
+}
+
+async fn update_configuration_from_wpad(options: &Options, auto_config_handler: Arc<AutoConfigHandler>) {
+    if options.force_proxy.is_none() {
+        let urls = match options.force_wpad_url {
+            Some(ref url) => [url.clone()].to_vec(),
+            None => wpad::get_wpad_urls().await.unwrap(),
+        };
+        let wpad = wpad::retrieve_first_working_url(urls).await.unwrap();
+        auto_config_handler.set_current_wpad_script(wpad);
+    }
 }
--- src/proxy/mod.rs
+++ src/proxy/mod.rs
@@ -1,9 +1,8 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, ToSocketAddrs};
+use std::net::ToSocketAddrs;
 use std::sync::Arc;
 
 use tokio;
 use tokio::io;
-use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 use tokio::prelude::*;
 
@@ -16,31 +15,13 @@ use self::connection::two_way_pipe;
 use self::protocol::Preamble;
 use crate::pacparser::ProxySuggestion;
 
-pub async fn serve<F>(port: u16, find_proxy: F)
-    where F: 'static+Send+Sync+Fn(&str, &str) -> ProxySuggestion
-{
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
-    let mut listener = TcpListener::bind(&addr).await.unwrap();
-
-    let find_proxy_arc = Arc::new(find_proxy);
-
-    loop {
-        if let Ok((downstream_connection, _)) = listener.accept().await {
-            debug!("accepted socket; addr={:?}", downstream_connection.peer_addr().unwrap());
-
-            let find_proxy_clone = find_proxy_arc.clone();
-            process_socket(downstream_connection, find_proxy_clone).await;
-        }
-    }
-}
-
 async fn send_error(conn: &mut TcpStream) -> io::Result<()> {
     conn.write_all(b"<h1>Could not connect</h1>").await?;
     // TODO(tkluck): close it; conn.close();
     Ok(())
 }
 
-async fn process_socket<F>(mut downstream_connection: TcpStream, find_proxy: Arc<F>) -> io::Result<()>
+pub async fn process_socket<F>(mut downstream_connection: TcpStream, find_proxy: Arc<F>) -> io::Result<()>
     where F: 'static+Send+Sync+Fn(&str, &str) -> ProxySuggestion
 {
 
